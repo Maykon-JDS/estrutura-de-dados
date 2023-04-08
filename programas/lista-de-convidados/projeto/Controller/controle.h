fa80f32b274d866a687b8ab9bcf721a2e9155dff
Maykon Dias <Maykondias2001@gmail.com>
2023-04-08 17:37:46 -0300

    Erro Buffer

--- programas/lista-de-convidados/projeto/Controller/controle.h
+++ programas/lista-de-convidados/projeto/Controller/controle.h
@@ -6,10 +6,11 @@ class Controller
 private:
     Logica *logica = new Logica();
     vector<string> guestRegistration;
-
+    
 public:
     int controlTheOptions(int opcao)
     {
+        string teste;
         int estadoDoSistema = 1;
 
         switch (opcao)
@@ -21,6 +22,9 @@ public:
 
         case 1:
             registerGuest();
+
+            // cin >> teste;
+            
             return estadoDoSistema;
             break;
 
@@ -54,51 +58,53 @@ private:
 
     void registerGuest()
     {
-        cout << "Nome: ";
+
         string nome;
-        cin >> nome;
-        guestRegistration.push_back(nome);
+        cout << "Nome: ";
+        getline(cin, nome);
 
-        cout << "\nVai colaborar trazendo algo?";
-        cout << "\n1 - Sim\n0 - Não\n";
-        cout << "\nNúmero da Opção: ";
+        // guestRegistration.push_back(nome);
+
+        // cout << "\nVai colaborar trazendo algo?";
+        // cout << "\n1 - Sim\n0 - Não\n";
+        // cout << "\nNúmero da Opção: ";
+
+        // int response;
+        // cin >> response;
 
 
-        int response;
-        cin >> response;
+        // int isActiveLoop = 1;
+        // for (int i = 1; isActiveLoop != 0;)
+        // {
+        //     if (response == 1)
+        //     {
+        //         string item;
+        //         cout << i << "º Item: ";
+        //         getline(cin, item);
+        //         guestRegistration.push_back(item);
 
-        int isActiveLoop = 1;
-        for (int i = 1; isActiveLoop != 0;)
-        {
-            if (response == 1)
-            {
-                string item;
-                cout << i << "º Item: ";
-                getline(cin, item);
-                guestRegistration.push_back(item);
+        //         cout << "\nDeseja adicionar mais itens?";
+        //         cout << "\n1 - Sim\n0 - Não\n";
+        //         cout << "\nNúmero da Opção: ";
+        //         cin >> response;
+        //         i++;
+        //     }
+        //     else if (response == 0)
+        //     {
+        //         cout << "\nTotal de itens adicionados: " << i;
+        //         isActiveLoop = 0;
+        //     }
+        //     else
+        //     {
+        //         cout << "\nDigite um valor valido!\n";
 
-                cout << "\nDeseja adicionar mais itens?";
-                cout << "\n1 - Sim\n0 - Não\n";
-                cout << "\nNúmero da Opção: ";
-                cin >> response;
-                i++;
-            }
-            else if (response == 0)
-            {
-                cout << "\nTotal de itens adicionados: " << i;
-                isActiveLoop = 0;
-            }
-            else
-            {
-                cout << "\nDigite um valor valido!\n";
+        //         cout << "\nDeseja adicionar mais itens?";
+        //         cout << "\n1 - Sim\n0 - Não\n";
+        //         cout << "\nNúmero da Opção: ";
+        //         cin >> response;
+        //     }
+        // }
 
-                cout << "\nDeseja adicionar mais itens?";
-                cout << "\n1 - Sim\n0 - Não\n";
-                cout << "\nNúmero da Opção: ";
-                cin >> response;
-            }
-        }
-
-        addPersonToGuestList(guestRegistration);
+        // addPersonToGuestList(guestRegistration);
     }
 };
